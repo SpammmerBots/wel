a53e158b2c37494d47024d1c6a877314be242c0f
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-06 02:11:24 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('guildMemberAdd', member => {
+const Mahmoud-QuaStyle= member.guild.channels.get("512674548382957573");
+if(!Mahmoud-QuaStyle) return;
+if(Mahmoud-QuaStyle) {
+setTimeout(() => Mahmoud-QuaStyle.send(`**Welcome To __ # - Mal Shop :rose: :heart: __ **`), 4000)        
+}
+});
+
+client.on('guildMemberAdd', member => {
+const Mahmoud-QuaStyle= member.guild.channels.get("513381450448371712");
+if(!Mahmoud-QuaStyle) return;
+if(Mahmoud-QuaStyle) {
+setTimeout(() => Mahmoud-QuaStyle.send(`**Welcome To __ # - Malicious™ <#519923132128165928> <#519935348823031808> :rose: :fire:  __ **`), 4000)        
+}
+});
+
+client.on('guildMemberAdd', member => {
+const Mahmoud-QuaStyle= member.guild.channels.get("516323087978070016");
+if(!Mahmoud-QuaStyle) return;
+if(Mahmoud-QuaStyle) {
+setTimeout(() => Mahmoud-QuaStyle.send(`**Welcome To __ # - Marley STORE :rose: :heart: __ **`), 4000)        
+}
+});
+
+
+
+
+
+
+   
+client.login(process.env.BOT_TOKEN);
